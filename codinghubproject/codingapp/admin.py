3f98a3e5cffa22b9302f27dacd3b8b3f6e6bd49d
tszarly-j <ssalijoshua2002@gmail.com>
2024-08-12 08:38:49 +0300

    created django admin

--- codinghubproject/codingapp/admin.py
+++ codinghubproject/codingapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import UserProfile, Challenge, TestCase, Solution, Comment, Tag
 
 # Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Challenge)
+admin.site.register(TestCase)
+admin.site.register(Solution)
+admin.site.register(Comment)
+admin.site.register(Tag)
--- codinghubproject/codingapp/models.py
+++ codinghubproject/codingapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, User
+from django.conf import settings
 
 # Create your models here.
 
@@ -16,7 +17,7 @@ class CustomUser(AbstractUser):
     
 # USER PROFILE MODEL TO SHOW USER DETAILS
 class UserProfile(models.Model):
-    user = models.One-To-OneField(User, on_delete=models.CASCADE) 
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) 
     bio = models.TextField(blank=True, null=True)
     score = models.IntegerField(default=0)
     rank = models.CharField(max_length=50, blank=True, null=True)
@@ -37,13 +38,13 @@ class Challenge(models.Model):
 
     tags = models.ManyToManyField('Tag', related_name='challenges', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
 class TestCase(models.Model):
-    challenge = models.ForeignKey(Challenge, related_name='solutions', on_delete=models.CASCADE)
+    challenge = models.ForeignKey(Challenge, related_name='test_cases', on_delete=models.CASCADE)
     input_data = models.TextField()
     expected_output = models.TextField()
 
@@ -52,9 +53,9 @@ class TestCase(models.Model):
     
 class Solution(models.Model):
     challenge = models.ForeignKey(Challenge, related_name='solutions', on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     code = models.TextField()
-    created_at = models.DataTimeField(auto_now_add = True)
+    created_at = models.DateTimeField(auto_now_add = True)
     is_accepted = models.BooleanField(default=False)
 
     def __str__(self):
@@ -62,7 +63,7 @@ class Solution(models.Model):
     
 class Comment(models.Model):
     solution = models.ForeignKey(Solution, related_name='comments', on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
