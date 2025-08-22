159159fca095a225448b5acbafc30060373097a0
Ssalijoshua <ssalijoshua2002@gmail.com>
2025-08-22 14:35:02 +0300

    Added requests

--- enryption_algorithms/algorithms.py
+++ enryption_algorithms/algorithms.py
@@ -1,6 +1,13 @@
 # User should be prompted for input
 message = input("Please enter your message:")
 
+
+print("1. Caesar cipher\n")
+print("2. Reverse cipher\n")
+print("3. Vernam cipher\n")
+
+encryption_algorithm = input("What encryption algorithm do you want to use?")
+
 # Encypt
 
 
